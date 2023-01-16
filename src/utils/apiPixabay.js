af134852a5ba81faaf70aa6994decd759aea255e
namhels <8.lsv.888@gmail.com>
2023-01-16 22:35:17 +0300

    start

---
+++ src/utils/apiPixabay.js
@@ -0,0 +1,34 @@
+// import axios from 'axios';
+
+// // https://pixabay.com/api/?q=cat&page=1&key=your_key&image_type=photo&orientation=horizontal&per_page=12
+
+// axios.defaults.baseURL = 'https://pixabay.com/api/';
+// const API_KEY = '25272385-d3b781fb1902e693cd197cf56';
+
+// export default class ApiPixabay {
+//   constructor() {
+//     this.page = 1;
+//   }
+
+//   async fetchImages(inputValue) {
+//     const queryParams = new URLSearchParams({
+//       key: API_KEY,
+//       q: inputValue,
+//       image_type: 'photo',
+//       orientation: 'horizontal',
+//       // safesearch: true,
+//       page: this.page,
+//       per_page: 12,
+//     });
+//     const { data } = await axios.get(`?${queryParams}`);
+//     return data;
+//   }
+
+//   incrementPage() {
+//     this.page += 1;
+//   }
+
+//   resetPage() {
+//     this.page = 1;
+//   }
+// }
